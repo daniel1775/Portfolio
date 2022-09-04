f51a64fc46486343c4594349eacd0ab6245e09b3
Daniel <danilog1000@gmail.com>
2022-09-04 11:56:51 -0500

    header diagonal effect fixed

--- src/views/Header/Header.jsx
+++ src/views/Header/Header.jsx
@@ -5,20 +5,16 @@ export default function Header(props) {
 	const { language, darkMode } = props;
 
 	return (
-		<header
-			className={`${style.header} ${
-				darkMode ? style.header_black : style.header_light
-			}`}
-		>
-			<div
-				className={`${style.titles} ${darkMode ? style.titles_light : style.titles_dark}`}
-			>
-				<h1>Hi, I am</h1>
-				<h1 className={style.titles_name}>Daniel Nieto</h1>
-				<h2>Full stack - developer</h2>
-			</div>
-			<div className={style.image}>
-				<img src={pc} alt="" />
+		<header className={`${style.header} ${darkMode ? style.header_black : style.header_light}`}>
+			<div className={style.header_container}>
+				<div className={`${style.titles} ${darkMode ? style.titles_light : style.titles_dark}`}>
+					<h1>Hi, I am</h1>
+					<h1 className={style.titles_name}>Daniel Nieto</h1>
+					<h2>Full stack - developer</h2>
+				</div>
+				<div className={style.image}>
+					<img src={pc} alt="" />
+				</div>
 			</div>
 		</header>
 	);
--- src/views/Navbar/components/Mode/Mode.jsx
+++ src/views/Navbar/components/Mode/Mode.jsx
@@ -1,32 +1,39 @@
-
 import style from './Mode.module.css';
 import { HiOutlineSun, HiMoon } from 'react-icons/hi';
 
-export default function Mode(props){
-  const { language, setLanguage, darkMode, setDarkMode } = props;
+export default function Mode(props) {
+	const { language, setLanguage, darkMode, setDarkMode } = props;
 
-  return(
-    <div className={style.mode}>
-      <button 
-        className={`${style.button_mode}`}
-      >
-        {darkMode ? 
-        <HiOutlineSun
-          className={`${style.icons_hiSun}`} 
-          onClick={() => setDarkMode(false)}
-        /> : 
-        <HiMoon 
-          className={style.icons_hiMoon} 
-          onClick={() => setDarkMode(true)}
-        />}
-      </button>
-      <div className={`${style.icons_line} ${darkMode ? style.line_light : style.line_dark}`}> | </div>
-      <button 
-        className={`${style.button_language} ${darkMode ? style.light_mode : style.dark_mode}`} 
-        onClick={() => setLanguage(!language)}
-      >
-        {language ? "ES" : "EN"}
-      </button>
-    </div>
-  );
+	return (
+		<div className={style.mode}>
+			<button className={`${style.button_mode}`}>
+				{darkMode ? (
+					<HiOutlineSun
+						className={`${style.icons_hiSun}`}
+						onClick={() => setDarkMode(false)}
+					/>
+				) : (
+					<HiMoon
+						className={style.icons_hiMoon}
+						onClick={() => setDarkMode(true)}
+					/>
+				)}
+			</button>
+			<div
+				className={`${style.icons_line} ${
+					darkMode ? style.line_light : style.line_dark
+				}`}
+			>
+				{' '}|{' '}
+			</div>
+			<button
+				className={`${style.button_language} ${
+					darkMode ? style.light_mode : style.dark_mode
+				}`}
+				onClick={() => setLanguage(!language)}
+			>
+				{language ? 'ES' : 'EN'}
+			</button>
+		</div>
+	);
 }
